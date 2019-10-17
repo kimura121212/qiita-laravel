52ecea5fee5beb42ba220d8c4994663d5e59d9ca
kimura121212 <ykimuranoi@gmail.com>
2019-10-17 16:00:52 +0900

    記事一覧画面実装

--- routes/web.php
+++ routes/web.php
@@ -10,10 +10,7 @@
 | contains the "web" middleware group. Now create something great!
 |
 */
-
-Route::get('/', function () {
-    return view('top');
-});
+Route::get('/', 'Auth\PostController@showTopPage')->name('top'); 
 
 Auth::routes();
 
